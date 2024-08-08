c6d6d527eaeb8b65a72d27e6a6b9768895651acf
Juanco <juanffo874@gmail.com>
2024-08-08 11:38:28 -0600

    Corrige el error en mongodb atlas

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 mongoose
   .connect("mongodb+srv://juancodb:2315juanco@cluster0.busmaue.mongodb.net/schoolcontroldb?retryWrites=true&w=majority&appName=Cluster0")
-  .then(() => console.log("Conectado a mongodb"))
+  .then(() => console.log("Conectado a mongodb")) //Se corrige el error en el mongodb atlas
   .catch((error) => console.log("Error al conectar", error));
 
 export default mongoose
